a72adaf921893a9e0e47bdb76055986519314416
oogily <78455442+oogily@users.noreply.github.com>
2024-04-23 23:24:06 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1YB_Pu_rLHGPxNEzw57e0-zuLtN4KaDFtlCg0FUKNOK0";
 
 
 /* ------------------------------------------------------------------- */
